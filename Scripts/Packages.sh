19af12f33803fcc03ba8a17f95ef288b7b6e56ee
naore <feyy6326@hotmail.com>
2026-04-11 12:30:36 +0800

    Update Packages.sh

--- Scripts/Packages.sh
+++ Scripts/Packages.sh
@@ -77,7 +77,7 @@ UPDATE_PACKAGE "quickfile" "sbwml/luci-app-quickfile" "main"
 UPDATE_PACKAGE "viking" "VIKINGYFY/packages" "main" "" "luci-app-timewol luci-app-wolplus"
 UPDATE_PACKAGE "vnt" "lmq8267/luci-app-vnt" "main"
 
-UPDATE_PACKAGE "filetransfer" "f8q8/luci-app-filetransfer" "master"
+UPDATE_PACKAGE "filetransfer" "coolsnowwolf/luci/applications/luci-app-filetransfer" "master"
 
 #更新软件包版本
 UPDATE_VERSION() {

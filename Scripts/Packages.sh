d99e95ab82a6d8e0f13033eb2864758b9eb04405
naore <feyy6326@hotmail.com>
2026-04-12 22:03:11 +0800

    Update Packages.sh

--- Scripts/Packages.sh
+++ Scripts/Packages.sh
@@ -77,8 +77,8 @@ UPDATE_PACKAGE "quickfile" "sbwml/luci-app-quickfile" "main"
 UPDATE_PACKAGE "viking" "VIKINGYFY/packages" "main" "" "luci-app-timewol luci-app-wolplus"
 UPDATE_PACKAGE "vnt" "lmq8267/luci-app-vnt" "main"
 
-# UPDATE_PACKAGE "luci-app-filetransfer" "jjm2473/openwrt-third" "main" "pkg"
-UPDATE_PACKAGE "luci-app-filetransfer" "coolsnowwolf/luci/applications/luci-app-filetransfer" "master" "pkg"
+UPDATE_PACKAGE "luci-app-filetransfer" "jjm2473/openwrt-third" "main" "pkg"
+# UPDATE_PACKAGE "luci-app-filetransfer" "coolsnowwolf/luci/applications/luci-app-filetransfer" "master" "pkg"
 # UPDATE_PACKAGE "filetransfer" "naore/luci-app-filetransfer" "master" "" "luci-app-filetransfer"
 
 #更新软件包版本
